aa66277de947e5f8fbee0ca7ff2ef9b3115b9149
Majdi <majdiuchiha@gmail.com>
2025-06-14 12:53:21 +0100

    impl dockercompose file

---
+++ AuthorBookParentMS/src/main/java/tn/esprit/authorbookparentms/AuthorBookParentMsApplication.java
@@ -0,0 +1,13 @@
+package tn.esprit.authorbookparentms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthorBookParentMsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthorBookParentMsApplication.class, args);
+    }
+
+}
---
+++ AuthorBookParentMS/src/test/java/tn/esprit/authorbookparentms/AuthorBookParentMsApplicationTests.java
@@ -0,0 +1,13 @@
+package tn.esprit.authorbookparentms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthorBookParentMsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
